5f3cf77079961ae16b7bb07afb7dc786d239f8f8
hdq <haqsir@163.com>
2020-03-28 21:06:19 +0800

    优化

--- src/utils/date.js
+++ src/utils/date.js
@@ -14,10 +14,38 @@ export const calcDate = (date1, date2) => {
     return {
         leave1,
         leave2,
-        leave3, 
+        leave3,
         days: days,
         hours: hours,
         minutes: minutes,
         seconds: seconds,
     }
+}
+
+/**
+ * 日期格式化
+ */
+export function dateFormat (date, format) {
+    format = format || 'yyyy-MM-dd hh:mm:ss';
+    if (date !== 'Invalid Date') {
+        let o = {
+            "M+": date.getMonth() + 1, //month
+            "d+": date.getDate(), //day
+            "h+": date.getHours(), //hour
+            "m+": date.getMinutes(), //minute
+            "s+": date.getSeconds(), //second
+            "q+": Math.floor((date.getMonth() + 3) / 3), //quarter
+            "S": date.getMilliseconds() //millisecond
+        }
+        if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
+            (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (let k in o)
+            if (new RegExp("(" + k + ")").test(format))
+                format = format.replace(RegExp.$1,
+                    RegExp.$1.length === 1 ? o[k] :
+                        ("00" + o[k]).substr(("" + o[k]).length));
+        return format;
+    }
+    return '';
+
 }
